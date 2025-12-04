5c1f840ea8d13a667c722b2995ea07b9e85fa45b
buianhtuan2207 <22130309@st.hcmuaf.edu.vn>
2025-12-04 19:58:17 +0700

    feat: create component Button,Banner and add banner in page home

---
+++ src/components/Button/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.scss"; 
+
+export default function Button({
+                                   children,
+                                   variant = "primary",   // primary | secondary | outline | text | danger
+                                   size = "medium",       // small | medium | large
+                                   className = "",
+                                   href = "",
+                                   ...props
+                               }) {
+    // Tạo class động
+    const classes = `btn btn--${variant} btn--${size} ${className}`.trim();
+
+    // Nếu có href → render thẻ <a>, không thì render <button>
+    return href ? (
+        <a href={href} className={classes} {...props}>{children}</a>
+    ) : (
+        <button type="button" className={classes} {...props}>{children}</button>
+    );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,20 @@
 import Header from "../../components/Header";
+import Banner from "../../components/Banner";
 
-function Home() {
+
+export default function Home() {
     return(
         <div>
             <Header />
-
+            <Banner
+                type="hero"
+                subtitle="Ưu đãi đặc biệt giảm 30%"
+                title="Chocolate Handmade<br/><span>Từ hạt cacao thượng hạng</span>"
+                description="Mỗi thanh chocolate là một tác phẩm nghệ thuật – làm thủ công, nguyên chất 100%"
+                buttonText="Khám phá ngay"
+                buttonLink="/promotion"
+                image="https://royce.vn/wp-content/uploads/2019/12/H8-550x550.jpg"
+            />
         </div>
     );
 }
-export default Home;
